91843246b7213fca84869f091a697ad0e25190b5
jame12945 <jame12945@gmail.com>
2022-11-09 14:15:47 +0700

    first commit

---
+++ BehavioralDesignPatterns/HeightSortStrategy.java
@@ -0,0 +1,10 @@
+package BehavioralDesignPatterns;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class HeightSortStrategy implements SortStrategy {
+	@Override
+	public void customSort(List<Person> people) {Collections.sort(people, Comparator.comparing( Person::getHeight));}
+}
---
+++ BehavioralDesignPatterns/SortStrategy.java
@@ -0,0 +1,8 @@
+package BehavioralDesignPatterns;
+
+// DO NOT MODIFY ANYTHING BELOW THIS LINE!!
+import java.util.List;
+
+public interface SortStrategy {
+    void customSort(List<Person> people);
+}
